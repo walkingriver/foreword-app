98cbeb243756b201469896dbefb5072057cce5d7
Mike Callaghan <mikecallaghan67@gmail.com>
2018-11-18 00:23:38 -0500

    Simplify layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,15 +26,35 @@ export class AppComponent {
       this.statusBar.backgroundColorByHexString('#C1A172');
       this.splashScreen.hide();
 
+      // Test via a getter in the options object to see if the passive property is accessed
+      let supportsPassive = false;
+      try {
+        const opts = Object.defineProperty({}, 'passive', {
+          get: function () {
+            supportsPassive = true;
+          }
+        });
+        window.addEventListener('testPassive', null, opts);
+        window.removeEventListener('testPassive', null, opts);
+      } catch (e) { }
+
       // workaround to make scroll prevent work in iOS Safari > 10
       try {
         const isPolyfill = polyfill({
           // use this to make use of the scroll behaviour
-          dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+          dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride,
+          iterationInterval: 2,
+          forceApply: true
         });
         console.log('IsPolyFill: ', isPolyfill);
-        window.addEventListener('touchmove', function () { }, { passive: false });
+        if (supportsPassive) {
+          window.addEventListener('touchmove', function () { }, { passive: false });
+        } else {
+          window.addEventListener('touchmove', function () { });
+        }
       } catch (e) { }
     });
   }
+
+
 }
