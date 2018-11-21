3d65e296d34c3e55cf0dd1dc86fb6d873f1a94eb
Mike Callaghan <mikecallaghan67@gmail.com>
2018-11-20 21:23:28 -0500

    Add skeleton pages to start page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   { path: '', redirectTo: 'Start', pathMatch: 'full' },
   { path: 'home/:order', loadChildren: './home/home.module#HomePageModule' },
   { path: 'Start', loadChildren: './start/start.module#StartPageModule' },
+  { path: 'Intro', loadChildren: './intro/intro.module#IntroPageModule' },
+  { path: 'Privacy', loadChildren: './privacy/privacy.module#PrivacyPageModule' },
+  { path: 'Terms', loadChildren: './terms/terms.module#TermsPageModule' },
+  { path: 'About', loadChildren: './about/about.module#AboutPageModule' },
 ];
 
 @NgModule({
