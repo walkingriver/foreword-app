29bd13daf9ef1268e910b9926ce56f023784cfa4
Mike Callaghan <mikecallaghan67@gmail.com>
2018-11-20 21:00:14 -0500

    Add start page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: '', redirectTo: 'Start', pathMatch: 'full' },
+  { path: 'home/:order', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'Start', loadChildren: './start/start.module#StartPageModule' },
 ];
 
 @NgModule({
